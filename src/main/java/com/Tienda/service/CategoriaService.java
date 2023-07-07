fc2a1ddc83080b910b133fcc6f4866c87e7edd1a
Valeria_Arias_Castro <107090508+Pinguinita1102@users.noreply.github.com>
2023-07-07 16:28:35 -0600

    Finalización semana 6

--- src/main/java/com/Tienda/service/CategoriaService.java
+++ src/main/java/com/Tienda/service/CategoriaService.java
@@ -7,14 +7,14 @@ public interface CategoriaService {
     
     //Metodo para consultar las categorias. El parametro define si se muestran solo las activas o todas
     public List<Categoria> getCategorias(boolean activos);
-    //Se obtiene una categoria por su ID
     
+    //Se obtiene una categoria por su ID
     public Categoria getCategoria(Categoria categoria);
     
     //Insertar: se inserta cuando el idCategoria está vacío (valor 0 o null)
     //Modificar: se modifica cuando el idCategoria no está vacío
     public void save(Categoria categoria);
     
-    //Se elimina un registro por su ID
+    //Se elimina un registro por su IdCategoria
     public void delete(Categoria categoria);
 }
